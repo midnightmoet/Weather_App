88c5a804479c72fdbfc58bf2f05a2e92370464a3
Lisa Miner <midnightmoet@gmail.com>
2023-05-27 19:58:34 -0400

    App functional, now to fix style/img issues

--- script.js
+++ script.js
@@ -10,13 +10,40 @@ const formEl = document.querySelector("form");
 
 formEl.addEventListener("submit", (e) =>{
     e.preventDefault();
-    const city = cityInputEl.value;
-    const state = stateInputEl.value;
-    getWeatherData(city, state);
+    const cityValue = cityInputEl.value;
+    const stateValue = stateInputEl.value;
+    getWeatherData(cityValue);
 
-    // console.log(city, state);
+    console.log(cityValue, stateValue);
 })
 
-function getWeatherData(city){
-    
+async function getWeatherData(cityValue, stateValue){
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&${stateValue}&units=imperial`);
+
+        if(!response.ok){
+            throw new Error("Weather data not available");
+        }
+
+        const data = await response.json();
+
+        const temperature = Math.round(data.main.temp);
+        const humidity = Math.round(data.main.humidity);
+        const windSpeed = Math.round(data.wind.speed);
+        const weatherDescription = data.weather[0].description;
+
+        weatherDataEl.innerHTML = `
+        <div class="weather-data">
+            <h2 class="weather-data__temperature">${temperature}°F</h2>
+            <p class="weather-data__description">${weatherDescription}</p>
+            <p class="weather-data__humidity">Humidity: ${humidity}%</p>
+            <p class="weather-data__wind-speed">Wind Speed: ${windSpeed} mph</p>
+        </div>
+        `;
+/**This console log is simply for testing within the console for the data */
+        // console.log(data);
+
+    }catch (error) {  
+        console.log(error);
+    }
 }
