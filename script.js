ac696177bd45e677c603298a039d490e2c9859c8
Lisa Miner <midnightmoet@gmail.com>
2023-05-28 16:27:21 -0400

    Little JS change

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const apikey = "46f80a02ecae410460d59960ded6e1c6";
 const weatherDataEl = document.getElementById("weather-data");
 const cityInputEl = document.getElementById("city-input");
+const stateInputEl = document.getElementById("state-input");
 const formEl = document.querySelector("form");
 
 formEl.addEventListener("submit", (e) => {
-  event.preventDefault();
+  e.preventDefault();
   const cityValue = cityInputEl.value;
   const stateValue = stateInputEl.value;
   getWeatherData(cityValue, stateValue);
