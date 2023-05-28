ed6bcc3ded215622b1f8568e03fed13500cc6e71
Lisa Miner <midnightmoet@gmail.com>
2023-05-27 20:30:54 -0400

    It has been a long day, completing tomorrow

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ formEl.addEventListener("submit", (e) =>{
     e.preventDefault();
     const cityValue = cityInputEl.value;
     const stateValue = stateInputEl.value;
-    getWeatherData(cityValue);
+    getWeatherData(cityValue, stateValue);
 
-    console.log(cityValue, stateValue);
+    // console.log(cityValue, stateValue);
 })
 
 async function getWeatherData(cityValue, stateValue){
@@ -22,7 +22,7 @@ async function getWeatherData(cityValue, stateValue){
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&${stateValue}&units=imperial`);
 
         if(!response.ok){
-            throw new Error("Weather data not available");
+            alert("Weather data not available");
         }
 
         const data = await response.json();
@@ -31,15 +31,22 @@ async function getWeatherData(cityValue, stateValue){
         const humidity = Math.round(data.main.humidity);
         const windSpeed = Math.round(data.wind.speed);
         const weatherDescription = data.weather[0].description;
+        const icon = data.weather[0].icon;
+        const details = [
+            `Feels Like: ${Math.round(data.main.feels_like)}°F`,
+            `Humidity: ${Math.round(data.main.humidity)}%`,
+            `Wind Speed: ${Math.round(data.wind.speed)} mph`
+    
+        ]
 
-        weatherDataEl.innerHTML = `
-        <div class="weather-data">
-            <h2 class="weather-data__temperature">${temperature}°F</h2>
-            <p class="weather-data__description">${weatherDescription}</p>
-            <p class="weather-data__humidity">Humidity: ${humidity}%</p>
-            <p class="weather-data__wind-speed">Wind Speed: ${windSpeed} mph</p>
-        </div>
-        `;
+        weatherDataEl.querySelector(
+            ".icon"
+          ).innerHTML = `<img src="http://openweathermap.org/img/wn/${icon}.png" alt="Weather Icon">`;
+        weatherDataEl.querySelector(".temperature").innerHTML = `${temperature}°F`;
+        weatherDataEl.querySelector(".description").innerHTML = weatherDescription;
+        weatherDataEl.querySelector(".details").innerHTML = details.join("<br>");
+
+        
 /**This console log is simply for testing within the console for the data */
         // console.log(data);
 
